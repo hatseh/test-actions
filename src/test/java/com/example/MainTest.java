94e5d32f9cab5eba1a5fde737c61a93cdf201e08
Maystros <lab@sehouate.com>
2023-11-14 13:14:12 +0100

    add test file

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @org.testng.annotations.Test
+    public void testMain() {
+        System.out.println("This is a test that as executed.");
+    }
+}
